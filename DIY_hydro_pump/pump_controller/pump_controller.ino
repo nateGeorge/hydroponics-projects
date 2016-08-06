72d038f1597ea07b57bc1cd2aee93af6cb9ad9b8
wordsforthewise <wordsforthewise@gmail.com>
2016-08-06 14:57:29 -0600

    feat: two pumps with 7 mins on 53 mins off

--- DIY_hydro_pump/pump_controller/pump_controller.ino
+++ DIY_hydro_pump/pump_controller/pump_controller.ino
@@ -1,10 +1,12 @@
 char* names[] = {"mini Tomato", "blueberry"};
 int pins[] = {2, 3};
-unsigned long timeOn[] = {2000000, 0}; // time that the pump stays on for each 10000 = 10s pin in order
-unsigned long timeOff[] = {3600000, 0}; // time pump stays off -- 3600000 = 1 hr
-unsigned long startTime[] = {0, 0}; // time pump was turned on, in ms
+unsigned long timeOn[] = {420000, 420000}; // time that the pump stays on for each 10000 = 10s
+// 420000 = 7 minutes
+unsigned long timeOff[] = {3180000, 3180000}; // time pump stays off -- 3600000 = 1 hr
+// 3180000 = 53 minutes
+unsigned long startTime[] = {0, millis()}; // time pump was turned on, in ms
 bool pumpOn[] = {true, false};
-bool disable[] = {false, true};
+bool disable[] = {false, false};
 
 int pumps = sizeof(pins)/sizeof(int);
 
